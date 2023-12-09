6e286837426fcdaac56a82d9440e9292703d4b67
Arafath Hossain <arafatrahat94@gmail.com>
2023-12-08 22:39:31 -0800

    now adding all the configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        purpleLightC: "#5858FA",
+        purpleC: "#150AA1",
+        primaryBgLight: "white",
+        secondaryBgLight: "#F1F1F1",
+        primaryBgDark: "#181818",
+        secondaryBgDark: "#282828",
+        grayC: "#494949",
+        darkborder: "#404040",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  darkMode: ["class", '[data-theme="dark"]'],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        dark: {
+          "base-100": "#181818",
+          neutral: "#181818",
+        },
+      },
+    ],
+  },
+};
